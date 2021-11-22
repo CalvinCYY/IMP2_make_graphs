ff2c0aee38882bed8bbdcf6c8484b32add883fd4
Calvin Yiu <chunnyyiu@hotmail.com>
2021-11-22 12:05:26 +0000

    added label options to graphs & fixed graph saving

--- log2graphs.py
+++ log2graphs.py
@@ -1,9 +1,9 @@
 from parse_logs import parse_log_files
 from make_graphs import plot_training_errors
 
-def log2graphs(log_file):
+def log2graphs(log_file, tr_label, te_label):
     data = parse_log_files(log_file)
-    plot_training_errors(data)
+    plot_training_errors(data, tr_label, te_label)
 
 if __name__ == "__main__":
-    log2graphs("path/to/log.txt")
+    log2graphs("/Users/bd20841/code/IMP2_make_graphs/logs/1000_dt5ab_iter_log.txt", tr_label="DT5AB_1000", te_label="DT3")
--- make_graphs.py
+++ make_graphs.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__))+'/../')
 
 import matplotlib.pyplot as plt
 
-def plot_training_errors(data, save=True):
+def plot_training_errors(data, tr_label='training_set', te_label='test_set', save=True):
     for key, values in data.items():
         epochs = []
         errors = []
@@ -14,10 +14,11 @@ def plot_training_errors(data, save=True):
             errors.append(error)
 
         plt.plot(epochs, errors, label=key)
-        plt.title(f'Training errors for {key}')
+        plt.title(f'{tr_label} errors for {key} prediction against {te_label}')
         plt.xlabel('Epochs')
         plt.ylabel('Loss (MAE)')
         plt.legend()
+        fig = plt.gcf()
         plt.show()
         if save:
-            plt.savefig(f'graphs/{key}_training_error.png')
+            fig.savefig(f'graphs/{tr_label}_{key}_training_error.png', format='png')
