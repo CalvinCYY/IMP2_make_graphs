344e01fd52e7248d0b5a956c304d468bd108efde
Calvin Yiu <chunnyyiu@hotmail.com>
2021-11-24 12:43:17 +0000

    added functions to display & save graphs, also allows max epoch to be set

--- log2graphs.py
+++ log2graphs.py
@@ -1,9 +1,9 @@
 from parse_logs import parse_log_files
 from make_graphs import plot_training_errors
 
-def log2graphs(log_file, tr_label, te_label):
+def log2graphs(log_file, tr_label, te_label, max_epoch=None, show=True, save=True):
     data = parse_log_files(log_file)
-    plot_training_errors(data, tr_label, te_label)
+    plot_training_errors(data, tr_label, te_label, max_epoch=max_epoch, show=show, save=save)
 
 if __name__ == "__main__":
-    log2graphs("/Users/bd20841/code/IMP2_make_graphs/logs/1000_dt5ab_iter_log.txt", tr_label="DT5AB_1000", te_label="DT3")
+    log2graphs("/Users/bd20841/code/IMP2_make_graphs/logs/1000_dt5ab_iter_log_2.txt", tr_label="DT5AB_1000", te_label="DT3", max_epoch=250)
--- make_graphs.py
+++ make_graphs.py
@@ -5,13 +5,15 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__))+'/../')
 
 import matplotlib.pyplot as plt
 
-def plot_training_errors(data, tr_label='training_set', te_label='test_set', save=True):
+def plot_training_errors(data, tr_label='training_set', te_label='test_set', max_epoch=None, show=True, save=True):
     for key, values in data.items():
         epochs = []
         errors = []
         for i, error in enumerate(values):
             epochs.append(i)
             errors.append(error)
+            if i == max_epoch:
+                break
 
         plt.plot(epochs, errors, label=key)
         plt.title(f'{tr_label} errors for {key} prediction against {te_label}')
@@ -19,6 +21,9 @@ def plot_training_errors(data, tr_label='training_set', te_label='test_set', sav
         plt.ylabel('Loss (MAE)')
         plt.legend()
         fig = plt.gcf()
-        plt.show()
+        if show:
+            plt.show()
         if save:
-            fig.savefig(f'graphs/{tr_label}_{key}_training_error.png', format='png')
+            if not os.path.exists(f'graphs/{tr_label}'):
+                os.makedirs(f'graphs/{tr_label}')
+            fig.savefig(f'graphs/{tr_label}/{tr_label}_{key}_training_error.png', format='png')
